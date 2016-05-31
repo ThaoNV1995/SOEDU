be934ae8aebe0c8a77992d7a6853e3cbb14bf721
NamCkiku <tranhoangnam11373@gmail.com>
2016-05-31 13:15:58 +0700

    add project

---
+++ SOEDU/SOEDU/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SOEDU
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Content/js/jquery-1.10.2.min.js",
+                        "~/Content/Plugins/bootstrap/dist/js/bootstrap.min.js",
+                        "~/Content/js/main.js",
+                        "~/Content/js/jquery.scrollUp.min.js"
+                        ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Plugins/bootstrap/dist/css/bootstrap.min.css",
+                "~/Content/css/Style.css",
+                "~/Content/Plugins/font-awesome-4.5.0/css/font-awesome.min.css",
+                "~/Content/Plugins/bootstrap-social-gh-pages/bootstrap-social.css",
+                "~/Content/css/Style.css"
+                ));
+        }
+    }
+}
