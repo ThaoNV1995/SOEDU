c85212f4627644e85082c4a60f9a41a3aee4721f
Strongking <strongking1271995@gmail.com>
2016-06-08 16:39:17 +0700

    Update API

--- SOEDU/SOEDU/app/app.module.ts
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
-((): void=> {
-    var app = angular.module("angularWithTS", ['ngRoute']);
-    app.config(angularWithTS.Routes.configureRoutes);
-})() 
